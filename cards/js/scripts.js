4f3089fe2f3d59c9627fb517b2d8741015c98f65
Derek Smith <derek@>
2018-08-13 15:59:45 -0700

    add js scripts for card variance

--- cards/js/scripts.js
+++ cards/js/scripts.js
@@ -1,4 +1,19 @@
 $(document).ready(function){
+  var value = ["1", "2", "3", "4","5","6","7","8","9","10","jack","queen","king"]
+  var suit = ["heart", "spade", "clover", "diamond"]
+
+  var deck = [];
+  value.forEach(function)(value){
+  };suit.forEach(function)(suit){
+    deck.push(value+ "of" +rank){
+
+    });
+  }
+   deck.forEach(function)(card){};
+   $("#card").
 
 
-}
+
+
+
+});
